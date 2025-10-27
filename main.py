cbe993516c54f9231c1cf7936fb960a9fabbd8e4
ethair28 <etthai1010@gmail.com>
2025-10-27 20:37:34 +0200

    limited the game loop to 60 fps

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def main():
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    dt = 0
+    max_fps = 60
+    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        dt = clock.tick(max_fps) / 1000
 if __name__ == "__main__":
     main()
